aa75c17e4df963ec0e16275ab2b03cefc52947d6
Sebastian Cieśliński <sebac1392@gmail.com>
2016-12-02 19:25:16 +0100

    Jumping from main Activity to item Activity by clicking text button

---
+++ app/src/main/java/hackathon/mms/app/domain/officeList/DistrictOfficeItemInformation.java
@@ -0,0 +1,24 @@
+package hackathon.mms.app.domain.officeList;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import hackathon.mms.app.R;
+
+
+/**
+ * Created by scieslinski on 12/2/2016.
+ */
+
+public class DistrictOfficeItemInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_district_office_item_information);
+
+
+    }
+}
