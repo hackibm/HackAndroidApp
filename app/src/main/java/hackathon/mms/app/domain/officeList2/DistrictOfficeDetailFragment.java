7ec09a8e96eac899481af22a4f70b2b0b4fc7fd7
ks <krzysiek175@gazeta.pl>
2016-12-03 13:23:12 +0100

    cos tam dziala

--- app/src/main/java/hackathon/mms/app/domain/officeList2/DistrictOfficeDetailFragment.java
+++ app/src/main/java/hackathon/mms/app/domain/officeList2/DistrictOfficeDetailFragment.java
@@ -1,9 +1,19 @@
 package hackathon.mms.app.domain.officeList2;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import hackathon.mms.app.domain.officeList2.dummy.DummyContent;
+import hackathon.mms.app.R;
+import hackathon.mms.app.infrastructure.repository.GraphQLRepository;
+import hackathon.mms.app.shared.model.DistrictOffice;
 
 /**
  * A fragment representing a single DistrictOffice detail screen.
@@ -21,7 +31,7 @@ public class DistrictOfficeDetailFragment extends Fragment {
     /**
      * The dummy content this fragment is presenting.
      */
-    private DummyContent.DummyItem mItem;
+    private DistrictOffice mItem;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -34,31 +44,34 @@ public class DistrictOfficeDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        if (getArguments().containsKey(ARG_ITEM_ID)) {
-//            // Load the dummy content specified by the fragment
-//            // arguments. In a real-world scenario, use a Loader
-//            // to load content from a content provider.
-//            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
-//
-//            Activity activity = this.getActivity();
-//            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-//            if (appBarLayout != null) {
-//                appBarLayout.setTitle(mItem.content);
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        View rootView = inflater.inflate(R.layout.districtoffice_detail, container, false);
-//
-//        // Show the dummy content as text in a TextView.
-//        if (mItem != null) {
-//            ((TextView) rootView.findViewById(R.id.districtoffice_detail)).setText(mItem.details);
-//        }
-//
-//        return rootView;
-//    }
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+
+            GraphQLRepository respository = new GraphQLRepository();
+            //repository.
+
+            mItem = new DistrictOffice(getArguments().getString(ARG_ITEM_ID), "Nazwaxxxx", null, null);//DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+            Log.i("ID--->", getArguments().getString(ARG_ITEM_ID));
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.getName());
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.districtoffice_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.districtoffice_detail)).setText(mItem.getName());
+        }
+
+        return rootView;
     }
 }
--- app/src/main/java/hackathon/mms/app/domain/officeList2/DistrictOfficeListActivity.java
+++ app/src/main/java/hackathon/mms/app/domain/officeList2/DistrictOfficeListActivity.java
@@ -1,15 +1,34 @@
 package hackathon.mms.app.domain.officeList2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import hackathon.mms.app.R;
 import hackathon.mms.app.domain.officeList.DistrictOfficeListFragment;
+import hackathon.mms.app.domain.officeList2.dummy.DummyContent;
+import hackathon.mms.app.infrastructure.repository.GraphQLRepository;
 import hackathon.mms.app.shared.model.DistrictOffice;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * An activity representing a list of DistrictOffices. This activity
@@ -25,7 +44,7 @@ public class DistrictOfficeListActivity extends AppCompatActivity implements Dis
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
      */
-//    private boolean mTwoPane;
+    private boolean mTwoPane;
 
 
     @Override
@@ -37,116 +56,134 @@ public class DistrictOfficeListActivity extends AppCompatActivity implements Dis
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
-        Fragment districtOfficeListFrag = DistrictOfficeListFragment.newInstance();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.districe_office_list_container, districtOfficeListFrag)
-        .commit();
+//        Fragment districtOfficeListFrag = DistrictOfficeListFragment.newInstance();
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .add(R.id.districe_office_list_container, districtOfficeListFrag)
+//        .commit();
 
 
 
         View recyclerView = findViewById(R.id.districtoffice_list);
         assert recyclerView != null;
-        //setupRecyclerView((RecyclerView) recyclerView);
+        setupRecyclerView((RecyclerView) recyclerView);
 
-//        if (findViewById(R.id.districtoffice_detail_container) != null) {
-//            // The detail container view will be present only in the
-//            // large-screen layouts (res/values-w900dp).
-//            // If this view is present, then the
-//            // activity should be in two-pane mode.
-//            mTwoPane = true;
-//        }
+        if (findViewById(R.id.districtoffice_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
     }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+
+        GraphQLRepository repo = new GraphQLRepository();
+       List<DistrictOffice> districtOffices = new ArrayList<DistrictOffice>();
+        SimpleItemRecyclerViewAdapter urzedyAdapter = new SimpleItemRecyclerViewAdapter(districtOffices);
+        recyclerView.setAdapter(urzedyAdapter);
+        Observable<DistrictOffice> observable = repo.getDistrictOffices();
+        observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .toList().subscribe(list ->
+                {   districtOffices.addAll(list);
+                    for(DistrictOffice doff : districtOffices){
+                        Log.i("District2" , "Office: " + doff.getId()+" "+doff.getName());
+
+                    }
+                    urzedyAdapter.notifyDataSetChanged();
+                    //recyclerView.
+                },
+                Throwable::printStackTrace);
+
+       // recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(districtOffices));
+    }
+
     @Override
     public void onDistrictOfficeSelected(DistrictOffice districtOffice) {
 
     }
 
-    public void openItemInfo(View view){
-        Intent intent = new Intent(DistrictOfficeListActivity.this, DistrictOfficeDetailActivity.class);
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-        startActivity(intent);
+        private final List<DistrictOffice> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<DistrictOffice> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.districtoffice_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+         //   holder.mIdView.setText(mValues.get(position).getId());
+            holder.mContentView.setText(mValues.get(position).getName());
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(DistrictOfficeDetailFragment.ARG_ITEM_ID, holder.mItem.getId());
+                        DistrictOfficeDetailFragment fragment = new DistrictOfficeDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.districtoffice_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, DistrictOfficeDetailActivity.class);
+                        intent.putExtra(DistrictOfficeDetailFragment.ARG_ITEM_ID, holder.mItem.getId());
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public DistrictOffice mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
 
     }
-//    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-//        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
-//    }
-//
-//    public class SimpleItemRecyclerViewAdapter
-//            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
-//
-//        private final List<DummyContent.DummyItem> mValues;
-//
-//        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
-//            mValues = items;
-//        }
-//
-//        @Override
-//        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//            View view = LayoutInflater.from(parent.getContext())
-//                    .inflate(R.layout.districtoffice_list_content, parent, false);
-//            return new ViewHolder(view);
-//        }
-//
-//        @Override
-//        public void onBindViewHolder(final ViewHolder holder, int position) {
-//            holder.mItem = mValues.get(position);
-//            holder.mIdView.setText(mValues.get(position).id);
-//            holder.mContentView.setText(mValues.get(position).content);
-//
-//            holder.mView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    if (mTwoPane) {
-//                        Bundle arguments = new Bundle();
-//                        arguments.putString(DistrictOfficeDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-//                        DistrictOfficeDetailFragment fragment = new DistrictOfficeDetailFragment();
-//                        fragment.setArguments(arguments);
-//                        getSupportFragmentManager().beginTransaction()
-//                                .replace(R.id.districtoffice_detail_container, fragment)
-//                                .commit();
-//                    } else {
-//                        Context context = v.getContext();
-//                        Intent intent = new Intent(context, DistrictOfficeDetailActivity.class);
-//                        intent.putExtra(DistrictOfficeDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-//
-//                        context.startActivity(intent);
-//                    }
-//                }
-//            });
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-//            return mValues.size();
-//        }
-//
-//        public class ViewHolder extends RecyclerView.ViewHolder {
-//            public final View mView;
-//            public final TextView mIdView;
-//            public final TextView mContentView;
-//            public DummyContent.DummyItem mItem;
-//
-//            public ViewHolder(View view) {
-//                super(view);
-//                mView = view;
-//                mIdView = (TextView) view.findViewById(R.id.id);
-//                mContentView = (TextView) view.findViewById(R.id.content);
-//            }
-//
-//            @Override
-//            public String toString() {
-//                return super.toString() + " '" + mContentView.getText() + "'";
-//            }
-//        }
+
     }
 
