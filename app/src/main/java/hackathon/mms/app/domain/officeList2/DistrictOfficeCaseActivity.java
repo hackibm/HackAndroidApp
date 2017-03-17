2d5f7dc708fa80f62b6429f7a5cb9c2001ae2d9c
Sebastian Cieśliński <sebac1392@gmail.com>
2017-03-17 16:13:38 +0100

    Spinner

---
+++ app/src/main/java/hackathon/mms/app/domain/officeList2/DistrictOfficeCaseActivity.java
@@ -0,0 +1,13 @@
+package hackathon.mms.app.domain.officeList2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DistrictOfficeCaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_case);
+    }
+}
