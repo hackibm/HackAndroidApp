25281f3e4f966200c77394667c5ebaf75f437bd6
d4w1dk <d4w1dk@d4w1dks-mbp.warszawa-park.pl.ibm.com>
2017-03-17 11:55:48 +0100

    Google Maps apps button action

--- app/src/main/java/hackathon/mms/app/domain/officeList/DistrictOfficeMainActivity.java
+++ app/src/main/java/hackathon/mms/app/domain/officeList/DistrictOfficeMainActivity.java
@@ -1,6 +1,7 @@
 package hackathon.mms.app.domain.officeList;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -20,11 +21,21 @@ public class DistrictOfficeMainActivity extends AppCompatActivity
         setContentView(R.layout.activity_district_office_list);
 
         buttonList = (Button) findViewById(R.id.dictrict_office_list_btn) ;
+        buttonCase = (Button) findViewById(R.id.dictrict_office_list_cases_btn) ;
 
         buttonList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DistrictOfficeMainActivity.this, DistrictOfficeListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonCase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("http://maps.google.com/maps?saddr=52.1879919,20.9461545,15&daddr=52.2084261,20.944414,12"));
                 startActivity(intent);
             }
         });
